5acd58182ebccee1d95510e24f9893dba33fcf6b
MRafiullah <mrafikhan2021@gmail.comvccgit config --global user.email mrafikhan2021@gmail.comgit config --global user.email mrafikhan2021@gmail.com>
2021-11-13 04:38:14 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,196 @@
+// CHAPTER 21 TO 26
+
+// STRING METHODS
+
+// QUESTION = 1 //
+
+
+// var firstName = prompt("Enter Your FirstName");
+// var lastName = prompt("Enter Your LastName");
+// document.write(firstName + " " + lastName);
+
+// QUESTION = 2 //
+
+
+// var names = prompt("Enter any name");
+// var name1 = names.length;
+// document.write(names + " = "+" "+ name1);
+
+// QUESTION = 3 //
+// var names = "Pakistani";
+// var name1 = names.indexOf("n");
+// document.write("Index of 'n'" + " "+ name1);
+
+
+// QUESTION = 4 //
+
+// var names = "Hell World";
+// var name1 = names.lastIndexOf("l");
+// document.write(" Last index of 'l'" + " "+ name1);
+
+// QUESTION = 5 //
+
+// var names = "Pakistani";
+// var name1 = names.charAt(3);
+// document.write("Character at  index '3'" + " "+ name1);
+
+// QUESTION = 6 //
+
+// var firstName = prompt("Enter Your FirstName");
+// var lastName = prompt("Enter Your LastName");
+// var raff = firstName.concat(lastName);
+// document.write(raff);
+
+// QUESTION = 7 //
+
+// var nick = "Hyderbad";
+// var nicki = nick.replace("Hyder","Islam");
+// document.write(nicki);
+
+// QUESTION = 8 //
+
+// var nic = "Ali and Sami are best friends. They play cricket and football together.";
+// var nici = nic.replace(/and/g ,"&");
+// document.write(nici);
+ 
+
+// QUESTION = 9 //
+
+// var hmm = "472";
+// var huh = Number(hmm);
+// document.write(huh);
+
+
+// QUESTION = 10 //
+
+// var names = prompt("Enter Your Name").toUpperCase();
+// document.write(names)
+
+// QUESTION = 11 //
+
+// var userFirstName =prompt("Enter Your Name");
+
+// var firsatname = userFirstName[0].toUpperCase();
+// var othername = userFirstName.slice(1).toLowerCase();
+// document.write(firsatname+othername);
+
+
+// QUESTION = 12 //
+
+
+// var num = 35.36;
+// var names =num.toString().replace(".", ""); 
+// document.write(names)
+
+
+// QUESTION = 13 //
+
+// var username = prompt("Enter special name");
+// var arr =["@","!",",",".","$"];
+// for(var a =0; a=arr.length )
+
+
+// QUESTION = 14 //
+
+// var user = prompt("enter item").toLowerCase()
+// // var isMatch= "no"
+// var items = ["cake " ,"coca cola " , "juice" , "biscuit"]
+//  for (var i = 0 ; i<items.length ; i++){
+//      if(user === items[i]){
+//          alert(user + " is available at index " + i +" in our bakery ")
+//     isMatch= "yes"
+//         }
+//  }if (isMatch == "no"){
+//      alert("not available")
+//  }
+// 
+
+// QUESTION = 15 //
+
+
+// QUESTION = 16 //
+
+
+
+
+
+// QUESTION = 17 //
+
+
+// var names = "Pakistan";
+// var name1=names.slice(-1);
+// document.write(name1);
+
+
+// QUESTION = 18 //
+
+// sir ap kara de
+
+
+// CHAPTER 26 TO 30//
+
+
+// QUESTION = 1//
+
+// var num = 3.45214
+// var num1 = Math.round(3.45214);
+// var num2 = Math.floor(3.45214);
+// var num3 = Math.ceil(3.45214);
+// document.write("Number:"+ num + "<br>" + "Round of value:"+ num1+"<br>"+
+// "Floor value of the number:" + num2 +"<br>"+"Ceil value of the number:"+ num3)
+
+
+
+// QUESTION = 2//
+
+
+//  var num = -2.673;
+//  var num1 = Math.round(-2.673);
+//  var num2 = Math.floor(-2.673);
+//  var num3 = Math.ceil(-2.673);
+//  document.write("Number:"+ num + "<br>" + "Round of value:"+ num1+"<br>"+
+// "Floor value of the number:" + num2 +"<br>"+"Ceil value of the number:"+ num3)
+
+// QUESTION = 3//
+
+
+// nhi keya 
+
+
+// QUESTION = 4//
+
+// var num = Math.random(4,6);
+// document.write(num)
+
+// // QUESTION = 5//
+
+
+
+// var head = 1;
+// var tail = 2;
+
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss)
+// if(floor === 0){
+//     document.write("0 <br> Random Coin Value: Head")
+// } else if(floor === 1)
+// {
+//     document.write("1 <br> Random Coin Value: Tails")
+// }
+
+// // QUESTION = 6//
+
+
+// var num = Math.random("Random number between 1 and 100" + 1,100);
+// document.write(num)
+
+
+
+// // QUESTION = 8//
+//  var num = Math.random(1,10);
+//  document.write(num)
+
+
+//                   // THE END//
+
+
